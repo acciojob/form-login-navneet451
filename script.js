21f3f64d59750ce712a0b14f96a9a74633ee7f0a
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-20 11:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element
+    var form = document.forms[0];
 
+    // Extract the values of First Name and Last Name using their name attributes
+    var firstName = form.elements["First Name"].value;
+    var lastName = form.elements["Last Name"].value;
+
+    // Show the alert with the first and last name
+    alert(firstName + " " + lastName);
 }
+
